3eb08f7477e82c932a850fd25f07289e77045b65
vixxxaz <99724809+vixxxaz@users.noreply.github.com>
2022-11-30 12:19:07 +0100

    layout sur *

--- src/App.js
+++ src/App.js
@@ -13,13 +13,12 @@ function App() {
     <Router>
       <Sidebar />
       <Routes>        
-        <Route path='/' element={<Layout />} >
+        <Route path='*' element={<Layout />} >
           <Route index element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Route>       
-      </Routes>
-       
+      </Routes>       
     </Router>
     </>
   );
