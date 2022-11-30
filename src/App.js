e11de54e09a4ef63e39879cafcc4ab19a3f66463
vixxxaz <99724809+vixxxaz@users.noreply.github.com>
2022-11-30 11:46:29 +0100

    page error out

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import ErrorPage from './components/ErrorPage.jsx';
+
 import Sidebar from './components/Sidebar';
 
 function App() {
@@ -12,15 +12,14 @@ function App() {
     <>
     <Router>
       <Sidebar />
-      <Routes>
-         
+      <Routes>        
         <Route path='/' element={<Layout />} >
           <Route index element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Route>       
       </Routes>
-      <Route path='*' element={<ErrorPage />} /> 
+       
     </Router>
     </>
   );
